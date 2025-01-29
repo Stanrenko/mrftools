fa9dc13bfa6d20f48e1ad6f0d38fa8833e4494b2
Constantin SLIOUSSARENKO <constantin.slioussarenko@gmail.com>
2025-01-29 17:25:42 +0100

    install change

--- install_mrftools.sh
+++ install_mrftools.sh
@@ -1,9 +1,13 @@
 conda env create --name $1 --file environment.yml
 conda activate $1 
 
-cd..
+cd ..
 git clone https://github.com/pehses/twixtools.git
-pip install twixtools/.
+cd twixtools
+pip install .
 
+cd ..
 git clone https://github.com/py-baudin/epgpy.git
 pip install epgpy/.
+cd epgpy
+pip install .
